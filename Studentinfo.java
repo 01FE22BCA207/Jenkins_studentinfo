c21f6e12f32d7e20ecc08a5dbcbb6b6125852abe
01FE22BCA207 <01fe22bca207@kletech.ac.in>
2024-02-21 13:18:25 +0530

    Code related to student information

---
+++ Studentinfo.java
@@ -0,0 +1,15 @@
+public class Studentinfo{
+    public void display(String name, String USN, String Branch, String Department ){
+        System.out.println("The Student deatils");
+        System.out.println("Student Name: " +name);
+        System.out.println("Student USN : " +USN);
+        System.out.println("Student Branch : " +Branch);
+        System.out.println("Student Department : " + Department);
+    }
+public static void main(String[] args){
+    Studentinfo s1=new Studentinfo();
+    s1.display("vijay","01FE22BCA207","BCA","ComputerApplication");
+   
+
+}
+}
